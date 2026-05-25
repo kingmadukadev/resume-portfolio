af5e2e7581e9be5640878b28e91c7edc63bed237
kingmadukadev <madukagodwin962@gmail.com>
2026-05-25 13:19:11 +0100

    Project Card

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,19 +1,54 @@
+import expenseTrackerImg from '../assets/images/projects/expensetrackerimage.png';
+
+
+
 export const projects = [
     {
         id: 1,
         title: 'Expense Tracker',
         description: 'A web application that allows users to track their expenses and manage their budget effectively.',
-        image: "",
+        image: expenseTrackerImg,
         category: 'Full Stack',
         technologies: ['React', 'Node.js', 'Express', 'MongoDB'],
-        demoUrl: '',
-        githubUrl: ''
+        demoUrl: 'https://expense-tracker-frontend-h215.onrender.com/',
+        githubUrl: 'https://github.com/kingmadukadev/expense-tracker',
+        metrics: '4 Active users'
     },
 
     {
         id: 2,
         title: 'Task Management App',
         description: 'A simple task management application to help users organize their daily activities.',
+        image: "",
+        category: 'Frontend',
+        technologies: ['React', 'JavaScript', 'CSS'],
+        demoUrl: '',
+        githubUrl: ''
+    },
+    {
+        id: 3,
+        title: 'Luela Travel',
+        description: 'A simple Web application to help travel agencies organize their daily activities.',
+        image: "",
+        category: 'Frontend',
+        technologies: ['React', 'JavaScript', 'CSS'],
+        demoUrl: '',
+        githubUrl: ''
+    },
+    {
+        id: 4,
+        title: 'Music App',
+        description: 'A web app for entertaiment',
+        image: "",
+        category: 'Frontend',
+        technologies: ['React', 'JavaScript', 'CSS'],
+        demoUrl: '',
+        githubUrl: ''
+    },
+    {
+        id: 5,
+        title: 'Somto Lens',
+        description: 'A web app portfolio for a videographer',
         image: "",
         category: 'Frontend',
         technologies: ['React', 'JavaScript', 'CSS'],
